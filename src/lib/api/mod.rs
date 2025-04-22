3ab2ba3cdeb51b9b5138e82c253530c39ca05666
Xerxes-2 <dspxue@gmail.com>
2025-04-22 23:51:07 +1000

    fix: correct rquest features and update walkdir version in Cargo.toml; adjust api_auth export in mod.rs

--- src/lib/api/mod.rs
+++ src/lib/api/mod.rs
@@ -4,7 +4,7 @@ mod misc;
 mod openai;
 
 pub use messages::api_messages;
+pub use misc::api_auth;
 pub use misc::api_submit;
 pub use misc::api_version;
-pub use misc::api_auth;
 pub use openai::api_completion;
