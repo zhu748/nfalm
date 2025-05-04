54ecec37e20da0ed8c3eaaf86eae9b397825b23d
Xerxes-2 <dspxue@gmail.com>
2025-05-05 03:16:54 +1000

    support multi positions in trie

--- src/middleware/stop_sequences.rs
+++ src/middleware/stop_sequences.rs
@@ -19,13 +19,16 @@ pub async fn apply_stop_sequences(resp: Response) -> Response {
     let trie =
         trie_rs::map::Trie::from_iter(f.stop_sequences.iter().cloned().map(|s| (s.to_owned(), s)));
     let search = trie.inc_search();
-    let mut position = Position::from(search);
+    let mut positions = vec![Position::from(search)];
     let mut stop = false;
     let stream = stream.flat_map(move |event| {
         if stop {
             return stream::iter(vec![]);
         }
-        let mut search = IncSearch::resume(&trie, position);
+        let mut searches = positions
+            .iter()
+            .map(|p| IncSearch::resume(&trie, *p))
+            .collect::<Vec<_>>();
         let data = match event {
             Ok(eventsource_stream::Event { data, .. }) => data,
             Err(e) => return stream::iter(vec![Err(e)]),
@@ -46,43 +49,51 @@ pub async fn apply_stop_sequences(resp: Response) -> Response {
         let input = text.as_bytes();
         let mut res = vec![Ok(event)];
         for i in 0..input.len() {
-            match search.query(&input[i]) {
-                None => {
-                    search.reset();
-                }
-                Some(a) if a.is_match() => {
-                    let seq = search.value().unwrap();
-                    // stop sequence found
-                    let result = String::from_utf8_lossy(&input[..i + 1]).to_string();
-                    stop = true;
-                    let event = StreamEvent::ContentBlockDelta {
-                        delta: ContentBlockDelta::TextDelta { text: result },
-                        index,
-                    };
-                    let content_block_stop = StreamEvent::ContentBlockStop { index };
-                    let message_delta = StreamEvent::MessageDelta {
-                        delta: MessageDeltaContent {
-                            stop_reason: Some(crate::types::message::StopReason::StopSequence),
-                            stop_sequence: Some(seq.to_string()),
-                        },
-                        usage: None,
-                    };
-                    let message_stop = StreamEvent::MessageStop;
+            searches = searches
+                .iter_mut()
+                .map(|s| {
+                    let prev = s.to_owned();
+                    match s.query(&input[i]) {
+                        None => vec![s.to_owned()],
+                        Some(a) if a.is_match() => {
+                            let seq = s.value().unwrap();
+                            // stop sequence found
+                            let result = String::from_utf8_lossy(&input[..i + 1]).to_string();
+                            stop = true;
+                            let event = StreamEvent::ContentBlockDelta {
+                                delta: ContentBlockDelta::TextDelta { text: result },
+                                index,
+                            };
+                            let content_block_stop = StreamEvent::ContentBlockStop { index };
+                            let message_delta = StreamEvent::MessageDelta {
+                                delta: MessageDeltaContent {
+                                    stop_reason: Some(
+                                        crate::types::message::StopReason::StopSequence,
+                                    ),
+                                    stop_sequence: Some(seq.to_string()),
+                                },
+                                usage: None,
+                            };
+                            let message_stop = StreamEvent::MessageStop;
 
-                    res = [event, content_block_stop, message_delta, message_stop]
-                        .iter()
-                        .map(|e| {
-                            let event = Event::default();
-                            let event = event.json_data(e).unwrap();
-                            Ok(event)
-                        })
-                        .collect::<Vec<_>>();
-                    break;
-                }
-                _ => {}
-            }
+                            res = [event, content_block_stop, message_delta, message_stop]
+                                .iter()
+                                .map(|e| {
+                                    let event = Event::default();
+                                    let event = event.json_data(e).unwrap();
+                                    Ok(event)
+                                })
+                                .collect::<Vec<_>>();
+                            vec![]
+                        }
+                        _ => vec![prev, s.to_owned()],
+                    }
+                })
+                .take_while(|v| !v.is_empty())
+                .flatten()
+                .collect();
         }
-        position = Position::from(search);
+        positions = searches.into_iter().map(Position::from).collect();
         return stream::iter(res);
     });
     let mut resp = Sse::new(stream)
